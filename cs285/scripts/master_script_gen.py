a8fdc3c25605cf7963294b079a729253e80e8fe1
Thomas Liao <tholiao@gmail.com>
2019-12-17 16:17:57 -0800

    Moved more things into utils

--- cs285/scripts/master_script_gen.py
+++ cs285/scripts/master_script_gen.py
@@ -1,20 +1,27 @@
 import itertools
-from typing import Dict 
+from typing import Dict
 
 import utils
 
 seeds = [285, 2020, 2]
 
 # Default arg first
-environments = ['HalfCheetah-v2', 'CartPole-v0', 'InvertedPendulum-v2']
-strategies = ['ordered_random', 'pure_random', 'sequential', 'constrained_random', 'mixed']
-terminal_values = ['learn', 0, 2, 5, 10, 50]
+environments = ["HalfCheetah-v2", "CartPole-v0", "InvertedPendulum-v2"]
+strategies = [
+    "ordered_random",
+    "pure_random",
+    "sequential",
+    "constrained_random",
+    "mixed",
+]
+terminal_values = ["learn", 0, 2, 5, 10, 50]
 batch_sizes = [1000, 2000, 3000, 4000]
 
 DEFAULT_ENV = environments[0]
 DEFAULT_STRAT = strategies[0]
 DEFAULT_TERM = terminal_values[2]
 DEFAULT_BATCH_SIZE = batch_sizes[3]
+
 
 def dict_product(dicts):
     """
@@ -26,28 +33,26 @@ def dict_product(dicts):
     """
     return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
 
+
 tmux_cmd = "tmux \
     new-session -s {name} '{py_cmd}'  \; \
     detach-client \n"
 
 template_cmds = {
-    'InvertedPendulum-v2': \
-        "python run_hw3_actor_critic.py --env_name InvertedPendulum-v2 -n 100 -b "\
-        "1000 --exp_name {name} -ntu 10 -ngsptu 10 -gpu -clr 5e-3 --seed {seed} "\
-        "--discount 0.95 -diff --sample_strategy {sample_strategy} "\
-        "--terminal_val {terminal_val} -tb 1000",
-    'HalfCheetah-v2': \
-        "python run_hw3_actor_critic.py --env_name HalfCheetah-v2 -gpu "\
-        "--ep_len 150 --discount 0.90 --scalar_log_freq 1 -n 150 -l 2 -s 32 "\
-        "-b 30000 -eb 1500 -lr 0.02 --exp_name {name} -ntu 10 "\
-        "-ngsptu 10 -diff --seed {seed} --num_critic_updates_per_agent_update 10 "\
-        "--sample_strategy {sample_strategy} --terminal_val {terminal_val} "\
-        "-tb {batch_size} ",
-    'CartPole-v0': \
-        "python run_hw3_actor_critic.py --env_name CartPole-v0 -n 100 -b 1000 "\
-        "--seed {seed} --exp_name {name} -ntu 10 -ngsptu 10 -diff -gpu "\
-        "--sample_strategy {sample_strategy} --terminal_val {terminal_val} "\
-        "-tb 1000 "
+    "InvertedPendulum-v2": "python run_hw3_actor_critic.py --env_name InvertedPendulum-v2 -n 100 -b "
+    "1000 --exp_name {name} -ntu 10 -ngsptu 10 -gpu -clr 5e-3 --seed {seed} "
+    "--discount 0.95 -diff --sample_strategy {sample_strategy} "
+    "--terminal_val {terminal_val} -tb 1000",
+    "HalfCheetah-v2": "python run_hw3_actor_critic.py --env_name HalfCheetah-v2 -gpu "
+    "--ep_len 150 --discount 0.90 --scalar_log_freq 1 -n 150 -l 2 -s 32 "
+    "-b 30000 -eb 1500 -lr 0.02 --exp_name {name} -ntu 10 "
+    "-ngsptu 10 -diff --seed {seed} --num_critic_updates_per_agent_update 10 "
+    "--sample_strategy {sample_strategy} --terminal_val {terminal_val} "
+    "-tb {batch_size} ",
+    "CartPole-v0": "python run_hw3_actor_critic.py --env_name CartPole-v0 -n 100 -b 1000 "
+    "--seed {seed} --exp_name {name} -ntu 10 -ngsptu 10 -diff -gpu "
+    "--sample_strategy {sample_strategy} --terminal_val {terminal_val} "
+    "-tb 1000 ",
 }
 
 
@@ -57,56 +62,40 @@ template_cmds = {
 
 # Search over environments
 env_sweep_set = "run_env_sweep.sh"
-with open(env_sweep_set, 'w+') as f:
-    for exp_flags in dict_product(dict(seed=seeds, 
-                                       env_name=environments, 
-                                       sample_strategy=[DEFAULT_STRAT], 
-                                       terminal_val=[DEFAULT_TERM], 
-                                       batch_size=[DEFAULT_BATCH_SIZE])):
+with open(env_sweep_set, "w+") as f:
+    for exp_flags in utils.get_exp_flags("env_sweep"):
         name = utils.get_exp_name(exp_flags)
-        exp_flags['name'] = name
-        exp_cmd = template_cmds[exp_flags['env_name']].format_map(exp_flags)
+        exp_flags["name"] = name
+        exp_cmd = template_cmds[exp_flags["env_name"]].format_map(exp_flags)
         exp_cmd = tmux_cmd.format(**(dict(name=name, py_cmd=exp_cmd)))
         f.write(exp_cmd)
 
 # Search over sampling strategies
-sample_sweep_set = 'run_sample_sweep.sh'
-with open(sample_sweep_set, 'w+') as f:
-    for exp_flags in dict_product(dict(seed=seeds, 
-                                       env_name=[DEFAULT_ENV], 
-                                       sample_strategy=strategies, 
-                                       terminal_val=[DEFAULT_TERM], 
-                                       batch_size=[DEFAULT_BATCH_SIZE])):
+sample_sweep_set = "run_sample_sweep.sh"
+with open(sample_sweep_set, "w+") as f:
+    for exp_flags in utils.get_exp_flags("sample_sweep"):
         name = utils.get_exp_name(exp_flags)
-        exp_flags['name'] = name
-        exp_cmd = template_cmds[exp_flags['env_name']].format_map(exp_flags)
+        exp_flags["name"] = name
+        exp_cmd = template_cmds[exp_flags["env_name"]].format_map(exp_flags)
         exp_cmd = tmux_cmd.format(**(dict(name=name, py_cmd=exp_cmd)))
         f.write(exp_cmd)
 
 # Search over terminal values
-terminal_val_sweep_set = 'run_terminal_val_sweep.sh'
-with open(terminal_val_sweep_set, 'w+') as f:
-    for exp_flags in dict_product(dict(seed=seeds, 
-                                       env_name=[DEFAULT_ENV], 
-                                       sample_strategy=[DEFAULT_STRAT], 
-                                       terminal_val=terminal_values, 
-                                       batch_size=[DEFAULT_BATCH_SIZE])):
+terminal_val_sweep_set = "run_terminal_val_sweep.sh"
+with open(terminal_val_sweep_set, "w+") as f:
+    for exp_flags in utils.get_exp_flags("terminal_val_sweep"):
         name = utils.get_exp_name(exp_flags)
-        exp_flags['name'] = name
-        exp_cmd = template_cmds[exp_flags['env_name']].format_map(exp_flags)
+        exp_flags["name"] = name
+        exp_cmd = template_cmds[exp_flags["env_name"]].format_map(exp_flags)
         exp_cmd = tmux_cmd.format(**(dict(name=name, py_cmd=exp_cmd)))
         f.write(exp_cmd)
 
 # Search over batch sizes
-batch_size_sweep_set = 'run_batch_size_sweep.sh'
-with open(batch_size_sweep_set, 'w+') as f:
-    for exp_flags in dict_product(dict(seed=seeds, 
-                                       env_name=[DEFAULT_ENV], 
-                                       sample_strategy=[DEFAULT_STRAT], 
-                                       terminal_val=[DEFAULT_TERM], 
-                                       batch_size=batch_sizes)):
+batch_size_sweep_set = "run_batch_size_sweep.sh"
+with open(batch_size_sweep_set, "w+") as f:
+    for exp_flags in utils.get_exp_flags("batch_size_sweep"):
         name = utils.get_exp_name(exp_flags)
-        exp_flags['name'] = name
-        exp_cmd = template_cmds[exp_flags['env_name']].format_map(exp_flags)
+        exp_flags["name"] = name
+        exp_cmd = template_cmds[exp_flags["env_name"]].format_map(exp_flags)
         exp_cmd = tmux_cmd.format(**(dict(name=name, py_cmd=exp_cmd)))
         f.write(exp_cmd)
--- cs285/scripts/utils.py
+++ cs285/scripts/utils.py
@@ -1,5 +1,94 @@
-from typing import Dict
+import itertools
+
+from typing import List, Dict
+
+seeds = [285, 2020, 2]
+environments = ["HalfCheetah-v2", "CartPole-v0", "InvertedPendulum-v2"]
+strategies = [
+    "ordered_random",
+    "pure_random",
+    "sequential",
+    "constrained_random",
+    "mixed",
+]
+terminal_values = ["learn", 0, 2, 5, 10, 50]
+batch_sizes = [1000, 2000, 3000, 4000]
+
+DEFAULT_ENV = environments[0]
+DEFAULT_STRAT = strategies[0]
+DEFAULT_TERM = terminal_values[2]
+DEFAULT_BATCH_SIZE = batch_sizes[3]
+
+def dict_product(dicts):
+    """
+    >>> list(dict_product(dict(number=[1,2], character='ab')))
+    [{'character': 'a', 'number': 1},
+     {'character': 'a', 'number': 2},
+     {'character': 'b', 'number': 1},
+     {'character': 'b', 'number': 2}]
+    """
+    return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
 
 def get_exp_name(exp_flags: Dict) -> str:
-    return '_'.join([f'{k}={v}' for k, v in exp_flags.items()])
+    return "_".join([f"{k}={v}" for k, v in exp_flags.items()])
 
+
+def _get_env_sweep_flags() -> List[Dict]:
+    return dict_product(
+        dict(
+            seed=seeds,
+            env_name=environments,
+            sample_strategy=[DEFAULT_STRAT],
+            terminal_val=[DEFAULT_TERM],
+            batch_size=[DEFAULT_BATCH_SIZE],
+        )
+    )
+
+
+def _get_batch_size_sweep() -> List[Dict]:
+    return dict_product(
+        dict(
+            seed=seeds,
+            env_name=[DEFAULT_ENV],
+            sample_strategy=[DEFAULT_STRAT],
+            terminal_val=[DEFAULT_TERM],
+            batch_size=batch_sizes,
+        )
+    )
+
+
+def _get_terminal_val_sweep_flags() -> List[Dict]:
+    return dict_product(
+        dict(
+            seed=seeds,
+            env_name=[DEFAULT_ENV],
+            sample_strategy=[DEFAULT_STRAT],
+            terminal_val=terminal_values,
+            batch_size=[DEFAULT_BATCH_SIZE],
+        )
+    )
+
+
+def _get_sample_sweep_flags() -> List[Dict]:
+    return dict_product(
+        dict(
+            seed=seeds,
+            env_name=[DEFAULT_ENV],
+            sample_strategy=strategies,
+            terminal_val=[DEFAULT_TERM],
+            batch_size=[DEFAULT_BATCH_SIZE],
+        )
+    )
+
+
+def get_exp_flags(exp_set: str) -> List[Dict]:
+    if exp_set == "env_sweep":
+        return _get_env_sweep_flags()
+    elif exp_set == "sample_sweep":
+        return _get_sample_sweep_flags()
+    elif exp_set == "terminal_val_sweep":
+        return _get_terminal_val_sweep_flags()
+    elif exp_set == "batch_size_sweep":
+        return _get_batch_size_sweep()
+    else:
+        raise ValueError("Experiment set unrecognized!")
